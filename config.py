e4808b78201ea568de1c115e2b366d971bfa31ee
Zorritotheboss <100345937+Zorritotheboss@users.noreply.github.com>
2022-03-03 20:24:38 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = ''
-PV_USERS = ['FriendXfriendss']
+BOT_TOKEN = '5125060630:AAGBe_8gMULq_GTncneNvaZvIkZZL_taQRI'
+PV_USERS = ['maikelmrojas','ventaorosking']
 #Database
 USERS = {}
 def saveDB():
